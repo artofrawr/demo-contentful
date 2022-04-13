a32e6c27346b11c12ddd5685248f0cb90716959e
Jens Fischer <jens@weareprofound.com>
2022-04-12 22:47:27 -0400

    remove bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '.env') })
 
-/* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
+module.exports = {
   publicRuntimeConfig: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
   trailingSlash: false,
   reactStrictMode: true,
-})
+}
