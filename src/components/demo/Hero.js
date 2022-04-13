027ddb563b84f09a5e3111b0e604cb3eb117f688
Jens Fischer <jens@weareprofound.com>
2022-04-12 22:43:21 -0400

    add social share image

--- src/components/demo/Hero.js
+++ src/components/demo/Hero.js
@@ -1,17 +1,13 @@
 import { string } from 'prop-types'
 
-const Hero = ({ title, copy, image }) => <div>HERO: {title}</div>
+const Hero = ({ title }) => <div>HERO: {title}</div>
 
 Hero.propTypes = {
   title: string,
-  copy: string,
-  image: string,
 }
 
 Hero.defaultProps = {
   title: '',
-  copy: '',
-  image: '',
 }
 
 export default Hero
